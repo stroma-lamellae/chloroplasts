ce43eba10693df7663d0920394d023be32bb0aec
Jesse-Daniel Belleau-Karaskewich <jdbkaraskewich@gmail.com>
2019-02-18 17:11:09 -0500

    Added sample of sending data to another server. Not sending the proper data or anything yet, but it's something!

--- hook_system/client/ClientServer/Controllers/PackageController.cs
+++ hook_system/client/ClientServer/Controllers/PackageController.cs
@@ -18,6 +18,7 @@ namespace ClientServer.Controllers
     {
         private readonly ClientServerContext _context;
         private readonly IHttpClientFactory _clientFactory;
+<<<<<<< HEAD
         private readonly IProcessingService _processingService;
 
         public PackageController(ClientServerContext context, 
@@ -27,6 +28,13 @@ namespace ClientServer.Controllers
             _context = context;
             _clientFactory = clientFactory;
             _processingService = processingService;
+=======
+
+        public PackageController(ClientServerContext context, IHttpClientFactory clientFactory)
+        {
+            _context = context;
+            _clientFactory = clientFactory;
+>>>>>>> 958925a... Added sample of sending data to another server. Not sending the proper data or anything yet, but it's something!
         }
 
         // GET: api/package
@@ -57,11 +65,22 @@ namespace ClientServer.Controllers
             _context.Packages.Add(package);
             await _context.SaveChangesAsync();
 
-            // Below initiates the upload. Maybe we don't want this to await?
-            // Probably, this will be initiated in some other place (possibly a timed service?)
-            await _processingService.InitiateUpload(package); 
+            // May need to fix this
+            await SendPackage(package);
 
             return CreatedAtAction(nameof(GetPackage), new { id = package.PackageId }, package);
+        }
+
+        // Sends a package to some server
+        // This is intended to be an example as to how we will send data to the processing server
+        // Right now, this is the incorrect data, and we will probably want to have this send
+        //   in a different location.
+        // TODO: Move this to a Service, and have it remove tasks from a queue
+        private async Task SendPackage(Package package)
+        {
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.PostAsync("http://localhost:3000/submit", new StringContent(JsonConvert.SerializeObject(package), Encoding.UTF8, "application/json"));
         }
     }
 }
--- hook_system/client/ClientServer/Startup.cs
+++ hook_system/client/ClientServer/Startup.cs
@@ -39,6 +39,9 @@ namespace ClientServer
             var connection = "Data Source=clientserver.db";
             services.AddDbContext<ClientServerContext>
                 (options => options.UseSqlite(connection));
+
+            // For communicating with the Processing Server
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
