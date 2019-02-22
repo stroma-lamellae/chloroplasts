2884e32f72d5cf7071c1c69fb396a9104ce3afde
Trevor Vanderee <tvand08@hotmail.com>
2019-02-22 14:54:39 -0500

    Jesse and i created a database model

--- hook_system/client/ClientServer/Models/Package.cs
+++ hook_system/client/ClientServer/Models/Package.cs
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 using System;
-=======
->>>>>>> 991fe02... Base structure for the Client Server. Some models, and an api to go along with those models.
+using System;
 using System.Collections.Generic;
 
 namespace ClientServer.Models
@@ -9,18 +7,11 @@ namespace ClientServer.Models
     public class Package
     {
         public long PackageId { get; set; }
-<<<<<<< HEAD
         public long AssignmentId { get; set; }
         public long ResultId { get; set; }
         public DateTime EstimatedCompletion { get; set; }
-       
         public virtual Assignment Assignment { get; set; }
         public virtual Result Result { get; set; }
         public virtual ICollection<Assignment> PreviousAssignments { get; set; }
-=======
-        public long CurrentAssignmentId { get; set; }
-        public Assignment CurrentAssignment { get; set; }
-        public List<Assignment> PreviousAssignments { get; set; }
->>>>>>> 991fe02... Base structure for the Client Server. Some models, and an api to go along with those models.
     }
 }
--- hook_system/client/ClientServer/Models/PreviousAssignment.cs
+++ hook_system/client/ClientServer/Models/PreviousAssignment.cs
@@ -5,7 +5,6 @@ namespace ClientServer.Models
         public long PreviousAssignmentId { get; set; }
         public long AssignmentId { get; set; }
         public long PackageId { get; set; }
-        
         public virtual Assignment Assignment { get; set; }
         public virtual Package Package { get; set; }
     }
