3d8444bfaf3f183fd488ba4873c855df6ab0f5c0
Jesse-Daniel Belleau-Karaskewich <jdbkaraskewich@gmail.com>
2019-02-23 19:19:42 -0500

    Updated DbContext to reflect our new models

--- hook_system/client/ClientServer/Models/ClientServerContext.cs
+++ hook_system/client/ClientServer/Models/ClientServerContext.cs
@@ -10,9 +10,17 @@ namespace ClientServer.Models
 
         }
 
-        public DbSet<Course> Courses { get; set; }
         public DbSet<Assignment> Assignments { get; set; }
-        public DbSet<Submission> Submissions { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Exclusion> Exclusions { get; set; }
+        public DbSet<Line> Lines { get; set; }
+        public DbSet<Match> Matches { get; set; }
         public DbSet<Package> Packages { get; set; }
+        public DbSet<PackageExclusion> PackageExclusions { get; set; }
+        public DbSet<PreviousAssignment> PreviousAssignments { get; set; }
+        public DbSet<Result> Results { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Submission> Submissions { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
