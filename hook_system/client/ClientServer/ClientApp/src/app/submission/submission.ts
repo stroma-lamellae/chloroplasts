ec97c1a6cc19a811aabdf6772fe6572c5bf98215
Jesse-Daniel Belleau-Karaskewich <jdbkaraskewich@gmail.com>
2019-02-26 00:00:11 -0500

    ClientApp: Submission page and service.

---
+++ hook_system/client/ClientServer/ClientApp/src/app/submission/submission.ts
@@ -0,0 +1,8 @@
+export class Submission {
+    SubmissionId: number;
+    AssignmentId: number;
+    StudentNumber: string;
+    StudentLastname: string;
+    StudentFirstname: string;
+    Files: FileList;
+}
