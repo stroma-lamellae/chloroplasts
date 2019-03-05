d3de1373bc98814f7b7c1c70510709a1e45505d8
Trevor Vanderee <tvand08@hotmail.com>
2019-03-05 17:08:07 -0500

    got authentication working for now

--- hook_system/client/ClientServer/ClientApp/src/app/app.component.ts
+++ hook_system/client/ClientServer/ClientApp/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import {User} from './shared/models/user';
+import { AuthenticationService } from './shared/services/authentication.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentUser: User;
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
+
 }
