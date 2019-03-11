8df59693da82a8e7efc467d5ce5cc78b04cc2444
Trevor Vanderee <tvand08@hotmail.com>
2019-03-11 15:04:58 -0400

    Changed project to use component specific styles as opposed to folder - negates the need to use bem architecture - fixed compilation issue that was overlooked

--- hook_system/client/ClientServer/ClientApp/src/app/app.component.ts
+++ hook_system/client/ClientServer/ClientApp/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { AuthenticationService } from './shared/services/authentication.service'
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   currentUser: User;
