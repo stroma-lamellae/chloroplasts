bdaca5bd1629c9b86b014348a2ca15ce57b40d1a
Trevor Vanderee <tvand08@hotmail.com>
2019-03-28 09:55:09 -0400

    made navbar changes

--- hook_system/client/ClientServer/ClientApp/src/app/app.routing.ts
+++ hook_system/client/ClientServer/ClientApp/src/app/app.routing.ts
@@ -8,6 +8,8 @@ import { DeauthGuard } from './core/guards/deauth.guard';
 import { DashboardComponent } from './modules/pages/dashboard/dashboard.component';
 import { LoginComponent } from './modules/pages/login/login.component';
 import { ArchivedCoursesComponent } from './modules/pages/archived-courses/archived-courses.component';
+import { BulkSubmissionComponent } from './modules/components/bulk-submission/bulk-submission.component';
+import { SubmissionComponent } from './modules/components/submission/submission.component';
 
 const appRoutes: Routes = [
   { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
@@ -18,7 +20,11 @@ const appRoutes: Routes = [
   },
   { path: 'login', component: LoginComponent, canActivate: [DeauthGuard] },
   { path: 'course', component: CourseComponent, canActivate: [AuthGuard] },
+  // Change to SearchComponent
+  { path: 'search', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'submission', component: SubmissionComponent, canActivate: [AuthGuard] },
   { path: 'package', component: PackageComponent, canActivate: [AuthGuard] },
+  { path: 'upload', component: BulkSubmissionComponent, canActivate: [AuthGuard] },
   { path: 'result', component: ResultComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '/' }
 ];
