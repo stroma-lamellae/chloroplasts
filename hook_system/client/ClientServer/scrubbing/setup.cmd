0194dcbbb0d88a8e9204333fd2763161508714b7
Jesse-Daniel Belleau-Karaskewich <jdbkaraskewich@gmail.com>
2019-04-24 02:42:50 -0400

    Scrubbing stuff now included in publish

---
+++ hook_system/client/ClientServer/scrubbing/setup.cmd
@@ -0,0 +1,4 @@
+python -m virtualenv env
+env\scripts\activate
+pip install -r requirements.txt
+deactivate
---
+++ hook_system/client/ClientServer/scrubbing/setup.sh
@@ -0,0 +1,4 @@
+python3 -m virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+deactivate
