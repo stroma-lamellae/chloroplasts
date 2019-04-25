0f88e2e5d5945a37562be8d37de63d503f4e844e
Emilia Rutkowski <rutkowski890@gmail.com>
2019-04-24 20:03:07 -0400

    actually fixed the Travis test

--- hook_system/client/ClientServer/ClientApp/src/app/modules/pages/upload/upload.component.spec.ts
+++ hook_system/client/ClientServer/ClientApp/src/app/modules/pages/upload/upload.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { UniquePipe } from '../../../shared/pipes/unique.pipe';
 import { UploadComponent } from './upload.component';
+import { FormsModule } from '@angular/forms';
+import { CourseService } from '../../../core/services/course.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('UploadComponent', () => {
   let component: UploadComponent;
@@ -8,7 +13,9 @@ describe('UploadComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UploadComponent ]
+      declarations: [ UploadComponent, UniquePipe ],
+      imports: [FormsModule, RouterTestingModule],
+      providers: [CourseService, HttpClient, HttpHandler]
     })
     .compileComponents();
   }));
